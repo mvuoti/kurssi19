5a0def283908f359351b590a5a3ebd4bf2314de9
mvuoti <vuoti@iki.fi>
2019-09-28 15:08:18 +0300

    osa2 + osa3teht22 eslint AirBnB

--- osa2/puhelinluettelo/src/phone_database.js
+++ osa2/puhelinluettelo/src/phone_database.js
@@ -1,32 +1,28 @@
-import axios from 'axios'
+import axios from 'axios';
 
-//const URL = "http://localhost:3001/api/persons"
-//const URL = "http://mvuoti-fs19-3step10.herokuapp.com/api/persons"
-const URL = "/api/persons"
+// const URL = "http://localhost:3001/api/persons"
+// const URL = "http://mvuoti-fs19-3step10.herokuapp.com/api/persons"
+const URL = '/api/persons';
 
-const getAll = () =>
-    axios
-        .get(URL)
-        .then(r => r.data)
+const getAll = () => axios
+  .get(URL)
+  .then((r) => r.data);
 
-const addNew = (person) =>
-    axios
-        .post(URL, person)
-        .then(r => r.data)
+const addNew = (person) => axios
+  .post(URL, person)
+  .then((r) => r.data);
 
-const update = (person) =>
-    axios
-        .put(`${URL}/${person.id}`, person)
-        .then(r => r.data)
+const update = (person) => axios
+  .put(`${URL}/${person.id}`, person)
+  .then((r) => r.data);
 
-const remove = (id) =>
-    axios
-        .delete(`${URL}/${id}`)
-        .then(r => id)
+const remove = (id) => axios
+  .delete(`${URL}/${id}`)
+  .then(() => id);
 
 export default {
-    getAll,
-    addNew,
-    update,
-    remove
-}
+  getAll,
+  addNew,
+  update,
+  remove,
+};
